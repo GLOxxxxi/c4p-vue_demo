3c82d13864f4f60cb766ca7be136706a2cad3db2
GLOxxxxi <1169550631@qq.com>
2021-10-26 08:38:03 +0800

    完善框架

--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+/* router 路由 请求地址管理 */
 import router from './router'
+/* element ui 前端组件 */
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+/* 提供axios请求 */
+import axios from 'axios'
 
 Vue.config.productionTip = false
-
+/* 允许跨域携带cookie */
+axios.defaults.withCredentials = true
+/* element ui */
+Vue.use(ElementUI)
+Vue.prototype.$axios = axios
 new Vue({
   router,
   render: h => h(App)
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,13 @@
 module.exports ={
     devServer: {
         // development server port 8000
+        // 设置前端端口
         port: 8081,
         // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
+        // 代理
         proxy : {
             '/api': {
+                // 配置跨域请求路径
                 target: 'http://localhost:8080',
                 ws: false,
                 changeOrigin : true,
